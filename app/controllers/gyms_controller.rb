6e61a3dccee58789c63e18a3430a8292cef01a98
Jonathan Essmann <jonathan.essmann@icloud.com>
2022-11-15 12:43:57 +0800

    routes

---
+++ app/controllers/gyms_controller.rb
@@ -0,0 +1,2 @@
+class GymsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :gyms do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:edit, :update, :index]
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  end
